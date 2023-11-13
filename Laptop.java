6a76a7346914c140898c6743e160d9d257874bdf
us lab <us.lab@thelangschool.net>
2023-11-13 10:57:27 -0500

    filled in computer types

---
+++ Laptop.java
@@ -0,0 +1,18 @@
+class Laptop{
+  String manufacturer;
+  String os;
+  int ram;
+  int storage;
+  String chipType;
+  double weight;
+  boolean touchscreen;
+  public Laptop(String manufacturer, String os, int ram, int storage, String chipType, double weight, boolean touchscreen){
+    this.manufacturer = manufacturer;
+    this.os = os;
+    this.ram = ram;
+    this.storage = storage;
+    this.chipType = chipType;
+    this.weight = weight;
+    this.touchscreen = touchscreen;
+  }
+}
