8ea7085e959e86a77bf2a0b7a45367237e6acc06
jimatlang <jim.stewart@thelangschool.org>
2023-11-13 10:21:49 -0500

    created Desktop and Laptop classes

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public Main(){
+        Laptop l1 = new Laptop("Macintosh", "MacOS", 8, 256, "Silicon", 2.5, false);
+        Desktop d1 = new Desktop("Dell", "Windows", 16, 1024, "Intel", 2, true);    
+    }
+}
+
+
